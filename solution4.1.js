46718b3d11e5e1eeb7ae3891acdc79d32a5e2250
ngochung1997 <61041580+ngochung1997@users.noreply.github.com>
2020-02-20 09:41:20 +0700

    Add files via upload

---
+++ solution4.1.js
@@ -0,0 +1,19 @@
+function range(star, end, step = star < end ? 1 : -1){
+    let array = [];
+    if(step > 0){
+        for(let i = star ; i <= end; i += step )array.push(i);
+    }else{
+        for (let i = star; i >= end; i += step)array.push(i);
+    }
+    return array;
+}
+function sum(array){
+    let total = 0 ;
+    for(let value of array){
+        total += value;
+    }
+    return total;
+}
+console.log(range(1,10));
+console.log(range(5,2,-1));
+console.log(sum(range(1,10)));
---
+++ solution4.2.js
@@ -0,0 +1,20 @@
+function reverseArray(array){
+    let output = [];
+    for(let i = array.length -1 ;i >= 0; i--){
+        output.push(array[i]);
+    }
+    return output;
+}
+function reverseArrayInPlace(array){
+    for(let i = 0; i < Math.floor(array.length / 2) ; i ++){
+        let old = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
+    }
+    return array;
+}
+let arrayValue = [1,2,3,4,5];
+reverseArrayInPlace(arrayValue);
+
+console.log(reverseArray(["Nguyen","Ngoc","Hung"]));
+console.log(arrayValue);
